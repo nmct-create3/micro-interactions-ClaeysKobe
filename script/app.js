abe2f8c427463ed1a37286788d810f1881565db2
ClaeysKobe <kobe.claeys@student.howest.be>
2022-10-06 14:45:33 +0200

    feat: change password to text via js

--- script/app.js
+++ script/app.js
@@ -3,7 +3,17 @@ function handleFloatingLabel() {
 }
 
 function handlePasswordSwitcher() {
-    
+    let passwordSwitch = document.querySelector('.js-toggle-password');
+    let passwordInput = document.querySelector('.js-input-field');
+    passwordSwitch.addEventListener('click', function () {
+        if (passwordSwitch.checked == true) {
+            // console.log('checked');
+            passwordInput.setAttribute('type', 'text');
+        } else {
+            // console.log('not checked');
+            passwordInput.setAttribute('type', 'password');
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
