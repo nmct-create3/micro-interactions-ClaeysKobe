6a909362239a6235f6238079323b82509aea04f0
ClaeysKobe <kobe.claeys@student.howest.be>
2022-10-06 15:37:20 +0200

    feat: error message

--- script/app.js
+++ script/app.js
@@ -1,23 +1,101 @@
+let email = {},
+    password = {},
+    signInButton;
+
+function getDOMElements() {
+    // password
+    password.input = document.querySelector('.js-input-field');
+    password.label = document.querySelector('.js-password-label');
+    password.switch = document.querySelector('.js-toggle-password');
+    password.errorMeassage = document.querySelector('.js-password-error');
+    // email
+    email.input = document.querySelector('.js-email-input');
+    email.label = document.querySelector('.js-email-label');
+    email.errorMeassage = document.querySelector('.js-email-error');
+    // button
+    // signInButton = document.querySelector('.js-sign-in-button');
+    // enable listeners
+    enableListeners();
+}
+
+const isValidEmailAddress = function(emailAddress) {
+    // Basis manier om e-mailadres te checken.
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailAddress);
+};
+
+function enableListeners() {
+    blurListenerEmail();
+    blurListenerPassword();
+    // clickButton();
+}
+
+function blurListenerEmail() {
+    email.input.addEventListener('blur', function () {
+        if (email.input.value == '') {
+            email.errorMeassage.innerHTML = 'This field is required';
+            email.input.classList.add('has-error');
+            email.label.classList.add('has-error');
+        } else if (!isValidEmailAddress(email.input.value)) {
+            email.errorMeassage.innerHTML = 'Please enter a valid email address';
+            email.input.classList.add('has-error');
+            email.label.classList.add('has-error');
+        } else {
+            email.errorMeassage.innerHTML = '';
+            email.input.classList.remove('has-error');
+            email.label.classList.remove('has-error');
+        }
+    });
+}
+
+function blurListenerPassword() {
+    password.input.addEventListener('blur', function () {
+        if (password.input.value == '') {
+            password.errorMeassage.innerHTML = 'This field is required';
+            password.input.classList.add('has-error');
+            password.label.classList.add('has-error');
+        } else {
+            password.errorMeassage.innerHTML = '';
+            password.input.classList.remove('has-error');
+            password.label.classList.remove('has-error');
+        }
+    });
+}
+
+function clickButton() {
+    signInButton.addEventListener('click', function () {
+        handleFloatingLabel();
+        handlePasswordSwitcher();
+    });
+}
+
 function handleFloatingLabel() {
    
 }
 
 function handlePasswordSwitcher() {
-    let passwordSwitch = document.querySelector('.js-toggle-password');
-    let passwordInput = document.querySelector('.js-input-field');
-    passwordSwitch.addEventListener('click', function () {
-        if (passwordSwitch.checked == true) {
+    password.switch.addEventListener('change', function () {
+        if (password.switch.checked == true) {
             // console.log('checked');
-            passwordInput.setAttribute('type', 'text');
+            password.input.setAttribute('type', 'text');
         } else {
             // console.log('not checked');
-            passwordInput.setAttribute('type', 'password');
+            password.input.setAttribute('type', 'password');
         }
     });
+
+    // ALTERNATIEF:
+    /* 
+    const passwordOptions = ['password', 'text'];
+
+    passwordToggle.addEventListener('change', function () {
+        passwordInput.type = passwordOptions[+this.checked];
+    });
+    */
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
+    getDOMElements();
     handleFloatingLabel();
     handlePasswordSwitcher();
 });
